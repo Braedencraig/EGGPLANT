16340dc07d9ba4ca49bbf2ca16181e37a3800274
Braeden Craig <braeden.craig@gmail.com>
2024-05-25 13:45:17 -0400

    improve seo

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import image from "../public/hero.png";
 
 export default function Meta() {
   return (
@@ -32,27 +31,30 @@ export default function Meta() {
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="description"
-        content={`Eggplant Music & Sound – Original Music.  Music Supervision.  Music Licensing.  Sound Design.  Voice Direction.`}
-      />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Eggplant Music + Sound" />
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta
         name="keywords"
-        content="Eggplant, Eggplant Studio, Eggplant Music, Eggplant Sound, Eggplant Music & Sound, Eggplant Music and Sound, Eggplant Original Music, Eggplant Music Supervision, Eggplant Music Licensing, Eggplant Sound Design, Eggplant Voice Direction, The Eggplant, Eggplant Music + Sound"
+        content="Eggplant, Eggplant Studio, Eggplant Music, Eggplant Sound, Eggplant Music & Sound, Eggplant Music and Sound, Eggplant Original Music, Eggplant Music Supervision, Eggplant Music Licensing, Eggplant Sound Design, Eggplant Voice Direction, The Eggplant, Eggplant Music + Sound Audio House, Audio, Music, Sound, Sound effects, Commercial Audio, Film Audio, Video Game Audio, Custom Composition, Sound design, audio mixing, dolby 5.1, voice direction, voice casting, music consultation, licensing, independent musician, Composer, foley, music for media, craft, SFX, editing, audio post, Toronto, advertising, creative audio, radio commercials, spotify commercials, cinema, cinema mix"
       />
       <meta
         name="description"
-        content="Eggplant Music & Sound – Original Music.  Music Supervision.  Music Licensing.  Sound Design.  Voice Direction."
+        content="Eggplant Music and Sound is a full service post-audio house located in Toronto, Canada. We are a company made up of passionate musicians, engineers, and audiophiles, who work hard to make things sound really, really good. From commercials, to films, to video games, Eggplant Music and Sound has a long history of creative problem solving, and successful execution. Whatever the project may be, you can rest assured, Eggplant Music and Sound will get you to the finish line. "
       />
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="og:title" content="Eggplant Music + Sound" />
+      <meta name="twitter:site" content="@eggplantms" />
+      <meta name="twitter:creator" content="@eggplantms" />
       <meta
         property="og:description"
-        content="Eggplant Music & Sound – Original Music.  Music Supervision.  Music Licensing.  Sound Design.  Voice Direction."
+        content="Eggplant Music and Sound is a full service post-audio house located in Toronto, Canada. We are a company made up of passionate musicians, engineers, and audiophiles, who work hard to make things sound really, really good. From commercials, to films, to video games, Eggplant Music and Sound has a long history of creative problem solving, and successful execution. Whatever the project may be, you can rest assured, Eggplant Music and Sound will get you to the finish line. "
       />
-      <meta property="og:image" content={image} />
+      <meta
+        property="og:image"
+        content={"https://theeggplant.com/EP_Logo.png"}
+      />
       <link rel="canonical" href="https://theeggplant.com/" />
     </Head>
   );
